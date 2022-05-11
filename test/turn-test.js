ee9106f65c611287b850762c3e90e32cd3580bc1
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-10 21:03:46 -0700

    test for giveFeedback method

--- test/turn-test.js
+++ test/turn-test.js
@@ -14,38 +14,54 @@ describe("turn", function(){
     const turn = new Turn();
 
     expect(turn).to.be.an.instanceof(Turn);
-  })
+  });
 
   it("should have users guess to the question", () => {
     const turn = new Turn("Disneyland");
 
     expect(turn.guess).to.equal("Disneyland");
-  })
+  });
 
   it("should have a card object for the current card in play", () => {
     const card = new Card();
     const turn = new Turn("question", card);
 
     expect(turn.currentCard).to.equal(card);
-  })
+  });
 
   it("should have method that returns the guess", () => {
     const turn = new Turn("Disneyland");
 
     expect(turn.returnGuess()).to.equal("Disneyland")
-  })
+  });
 
   it("should have method that returns the Card object", () => {
     const card = new Card();
     const turn = new Turn("Disneyland", card);
 
     expect(turn.returnCard()).to.equal(card);
-  })
+  });
 
   it("should return boolean indicating if guess matches correct answer", () => {
     const card = new Card(1, "What is the happiest place on earth?", ["the Beach", "Jamaica", "Disneyland"], "Disneyland");
     const turn = new Turn("Disneyland", card);
 
     expect(turn.evaluateGuess()).to.deep.equal(true);
+  });
+
+  it("should give feedback depending on correct or incorrect answer", () => {
+    let card = new Card(1, "What is the happiest place on earth?", ["the Beach", "Jamaica", "Disneyland"], "Disneyland");
+    let turn = new Turn("Disneyland", card);
+
+    expect(turn.evaluateGuess()).to.deep.equal(true);
+    expect(turn.giveFeedback()).to.equal("Correct!");
+
+    let card = new Card(1, "What is the happiest place on earth?", ["the Beach", "Jamaica", "Disneyland"], "the Beach");
+    let turn = new Turn("Disneyland", card);
+
+    expect(turn.evaluateGuess()).to.deep.equal(false);
+    expect(turn.giveFeedback()).to.equal("Incorrect!");
+    
   })
+
 })
