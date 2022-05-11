13f1adfde2e0d00891f00aa8bc695c6ddb4dff17
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-10 20:12:17 -0700

    Test to see instance of turn

--- test/turn-test.js
+++ test/turn-test.js
@@ -7,6 +7,12 @@ describe("turn", function(){
   it("should be a function", () => {
     const turn = new Turn();
     expect(Turn).to.be.a("function");
+  });
+
+  it("should be an instance of Turn", () => {
+    const turn = new Turn();
+
+    expect(turn).to.be.an.instanceof(Turn);
   })
 
 })
