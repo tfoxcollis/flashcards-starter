22424340a72af99f07fd3f05ab25b2365229656f
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-10 21:58:51 -0700

    create testto check card count

--- test/deck-test.js
+++ test/deck-test.js
@@ -20,4 +20,15 @@ describe("deck", function(){
     expect(deck).to.have.key("cards");
     expect(deck.cards).to.deep.equal(cards);
   })
+
+  it("should know how many cards are in the deck", () => {
+    var cards = [
+      new Card(1, "What is James' favorite color?", ["red", "blue", "green"], "red"),
+      new Card(2, "What is James' favorite food?", ["burrito", "donut", "pot pie"], "burrito"),
+      new Card(3, "What is James' favorite vacation destination?", ["Denmark", "Jamaica", "Anywhere with his wife"], "Anywhere with his wife" )
+    ]
+    const deck = new Deck(cards);
+
+    expect(deck.countCards).to.equal(3);
+  })
 })
