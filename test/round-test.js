e9d7d903f9cb493ce8d8d23f4824839278c88d3e
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-10 22:29:00 -0700

    create test to confirm round starts with deck of cards

--- test/round-test.js
+++ test/round-test.js
@@ -12,4 +12,16 @@ describe("Round", function() {
     expect(Round).to.be.a("function");
   })
 
+  it("should start with a deck", () => {
+    var cards = [
+      new Card(1, "What is James' favorite color?", ["red", "blue", "green"], "red"),
+      new Card(2, "What is James' favorite food?", ["burrito", "donut", "pot pie"], "burrito"),
+      new Card(3, "What is James' favorite vacation destination?", ["Denmark", "Jamaica", "Anywhere with his wife"], "Anywhere with his wife" )
+    ]
+    const deck = new Deck(cards);
+    const round = new Round(deck);
+
+    expect(round.deck).to.deep.equal(deck.cards);
+  })
+
 })
