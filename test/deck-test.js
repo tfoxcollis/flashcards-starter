2704e8bfe2174a76dce1adc1cab4e5d43663d2ce
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-10 21:51:03 -0700

    create test for deck initialization with array

--- test/deck-test.js
+++ test/deck-test.js
@@ -9,4 +9,14 @@ describe("deck", function(){
       expect(Deck).to.be.a("function");
   })
 
+  it("should initialize with an array of card objects", () => {
+    var cards = [
+      new Card(1, "What is James' favorite color?", ["red", "blue", "green"], "red"),
+      new Card(2, "What is James' favorite food?", ["burrito", "donut", "pot pie"], "burrito"),
+      new Card(3, "What is James' favorite vacation destination?", ["Denmark", "Jamaica", "Anywhere with his wife"], "Anywhere with his wife" )
+    ]
+    const deck = new Deck(cards);
+
+    expect(deck).to.have.key("cards");
+  })
 })
