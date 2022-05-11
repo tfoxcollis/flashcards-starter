1876c95ea287b924060b2eab7d33b0b782edf85a
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-11 16:47:56 -0700

    test to check for empty incorrectGuesses array

--- src/round.js
+++ src/round.js
@@ -14,3 +14,16 @@ class Round{
 
 
 module.exports = Round
+
+// takeTurn method needs to:
+// update turn count
+// evaluate guesses
+//give giveFeedback
+// stores id of incorrect guesses
+
+
+//test for:
+// array for incorrectGuesses
+// turnCount being updated with each turnCount
+//    regardless of correct or incorrect answer (2 expects)
+//
--- test/round-test.js
+++ test/round-test.js
@@ -16,26 +16,32 @@ describe("Round", function() {
   it("should be a function", () => {
 
     expect(Round).to.be.a("function");
-  })
+  });
 
   it("should start with a deck", () => {
     const deck = new Deck(cards);
     const round = new Round(deck);
 
     expect(round.deck).to.deep.equal(deck.cards);
-  })
+  });
 
   it("should have method that returns the current card being played", () => {
     const deck = new Deck(cards);
     const round2 = new Round(deck);
 
     expect(round2.returnCurrentCard()).to.equal(cards[0]);
-  })
+  });
 
   it("should have a turn count that starts at 0", () => {
     const deck = new Deck(cards);
-    const round3 = new Round(deck);
+    const round = new Round(deck);
 
-    expect(round3.turnCount).to.equal(0);
+    expect(round.turnCount).to.equal(0);
+  });
+
+  it("should have an empty array to store incorrect guesses", () => {
+    const round = new Round();
+
+    expect(round.incorrectGuesses).to.equal([]);
   })
 })
