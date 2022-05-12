9b98fd65d244da93d808d8e2ef92b198ee426fef
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-05-12 16:23:33 -0700

    Refactor deck test to dry up code

--- test/deck-test.js
+++ test/deck-test.js
@@ -3,29 +3,27 @@ const expect = chai.expect;
 const Deck = require('../src/deck');
 const Card = require('../src/card');
 
+//Global variables
+var cards = [
+  new Card(1, "What is James' favorite color?", ["red", "blue", "green"], "red"),
+  new Card(2, "What is James' favorite food?", ["burrito", "donut", "pot pie"], "burrito"),
+  new Card(3, "What is James' favorite vacation destination?", ["Denmark", "Jamaica", "Anywhere with his wife"], "Anywhere with his wife" )
+]
+
 describe("deck", function(){
 
   it("should be a function", () => {
-      expect(Deck).to.be.a("function");
+
+    expect(Deck).to.be.a("function");
   })
 
   it("should initialize with an array of card objects", () => {
-    var cards = [
-      new Card(1, "What is James' favorite color?", ["red", "blue", "green"], "red"),
-      new Card(2, "What is James' favorite food?", ["burrito", "donut", "pot pie"], "burrito"),
-      new Card(3, "What is James' favorite vacation destination?", ["Denmark", "Jamaica", "Anywhere with his wife"], "Anywhere with his wife" )
-    ]
     const deck = new Deck(cards);
 
     expect(deck.cards).to.deep.equal(cards);
   })
 
   it("should know how many cards are in the deck", () => {
-    var cards = [
-      new Card(1, "What is James' favorite color?", ["red", "blue", "green"], "red"),
-      new Card(2, "What is James' favorite food?", ["burrito", "donut", "pot pie"], "burrito"),
-      new Card(3, "What is James' favorite vacation destination?", ["Denmark", "Jamaica", "Anywhere with his wife"], "Anywhere with his wife" )
-    ]
     const deck = new Deck(cards);
 
     expect(deck.countCards()).to.equal(3);
